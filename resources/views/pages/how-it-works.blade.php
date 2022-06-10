03b7719492278f5c6f66f4a21059ed19508d0b81
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-06-10 18:59:27 +0530

    how-it-works

--- resources/views/pages/how-it-works.blade.php
+++ resources/views/pages/how-it-works.blade.php
@@ -3,12 +3,131 @@
    <div class="container">
       <div class="row">
          <div class="col-12">
-            <h1 class="text-center mb-4">
-               <span class="paragraph netural-100">Make your special wedding with us</span><br/>
-               <span class="h1 netural-100">How to get married in Australia</span>
-            </h1>
+            <p class="paragraph netural-100 text-center mt-4">Make your special wedding with us</p>
+            <h1 class="h1 netural-100 text-center mb-4">How to get married in Australia</h1>
             <p class="col-10 mx-auto text-center mb-5">Getting married in Australia can be very straight forward and a lot more engaging than the Marriage Registry Office. With a simple process and an amazing marriage celebrant, you will love your experience</p>
             <img src="images/how-it-works-main.png" class="img-fluid" alt="How It Works Main Hero Image"> 
+         </div>
+      </div>
+   </div>
+</section>
+<section class="marrige-steps pt-100 pb-80">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <p class="paragraph mb-0 text-center neutral-100">Simple step</p>
+            <h2 class="h2 mb-75 neutral-100 text-center">Just 3 Steps to get married</h2>
+         </div>
+         <div class="col-md-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 1</span>
+               <div class="steps-name d-flex mt-4 pt-5 align-items-center">
+                  <img src="/images/marriage-calander-icon.svg" class="" alt="Marriage Calander">
+                  <h3 class="text-white h3">Make a <br>booking</h3>
+               </div>
+               <p class="body-3 text-white mb-0">From just $270. See available location, date & time</p>
+               <div class="step-description mt-4">
+                  <h4 class="h4 text-white text-center mb-4">Simply select, online</h4>
+                  <ul class="list-unstyled mb-0">
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">One of our beautiful, convenient marriage locations</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">Your wedding date</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">The time of your ceremony</p>
+                     </li>
+                  </ul>
+               </div>
+               <div class="step-description mt-4 mb-4">
+                  <h4 class="h4 text-white text-center mb-4">Highlights</h4>
+                  <ul class="list-unstyled mb-0">
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">Book online - 24/7</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">No interview required</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">All inclusive fee </p>
+                     </li>
+                  </ul>
+               </div>
+               <a href="#" class="theme-btn secondary-btn d-block">Learn more</a>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 2</span>
+               <div class="steps-name d-flex mt-4 pt-5 align-items-center">
+                  <img src="/images/marriage-calander-icon.svg" class="" alt="Marriage Calander">
+                  <h3 class="text-white h3">Complete <br>NoIM</h3>
+               </div>
+               <p class="body-3 text-white mb-0">Complete 1 month before your ceremony. No meeting or interview required!</p>
+               <div class="step-description mt-4">
+                  <p class="text-white text-center body-2 mb-0">Complete your Notice of Intended Marriage form online (we will guide you the whole way).<br><br>
+                     We are the celebrant to offer this. You can complete NoIM on your phone, if you wish!
+                  </p>
+               </div>
+               <div class="step-description mt-4 mb-4">
+                  <h4 class="h4 text-white text-center mb-4">Highlights</h4>
+                  <ul class="list-unstyled mb-0">
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">1 month prior to ceremony date</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">All paperwork done online</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">Only 1 party needs to sign, to complete the NoIM</p>
+                     </li>
+                  </ul>
+               </div>
+               <a href="#" class="theme-btn secondary-btn d-block">Learn more</a>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 3</span>
+               <div class="steps-name d-flex mt-4 pt-5 align-items-center">
+                  <img src="/images/marriage-calander-icon.svg" class="" alt="Marriage Calander">
+                  <h3 class="text-white h3">Your ceremony</h3>
+               </div>
+               <p class="body-3 text-white mb-0">Amazing location & celebrant! See how your day will flow</p>
+               <div class="step-description mt-4">
+                  <p class="text-white text-center body-2 mb-0">
+                     Stress free marriage ceremony with your amazing, experienced, warm and fun celebrant. Your marriage will be registered – on the same day!
+                  </p>
+               </div>
+               <div class="step-description mt-4 mb-4">
+                  <h4 class="h4 text-white text-center mb-4">Highlights</h4>
+                  <ul class="list-unstyled mb-0">
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">Warm personal ceremony</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">Amazing locations</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0">Same day registration</p>
+                     </li>
+                  </ul>
+               </div>
+               <a href="#" class="theme-btn secondary-btn d-block">Learn more</a>
+            </div>
          </div>
       </div>
    </div>
@@ -17,10 +136,8 @@
    <div class="container">
       <div class="row">
          <div class="col-12">
-            <h2 class="text-white text-center mb-75">
-               <p class="paragraph text-white mb-0">Our values</p>
-               <p class="h2 text-white mb-0">Why simple<br/> ceremonies?</p>
-            </h2>
+            <p class="paragraph text-white mb-0 text-center">Our values</p>
+            <h2 class="h2 text-white text-center mb-75">Why simple<br/> ceremonies?</h2>
          </div>
          <div class="col-md-6 col-lg-4 d-flex mb-5">
             <div class="me-3"><img src="images/ceremonies-icon/v1.svg" alt="ceremonies-icon"></div>
@@ -147,10 +264,8 @@
             <img src="images/permium.png" alt="permium" class="img-fluid">
          </div>
          <div class="col-md-7">
-            <h2 class="mb-4">
-               <p class="paragraph neutral-100">While offering amazing value</p>
-               <p class="h2 neutral-100">The Simple, Premium & Cool ways to get married!</p>
-            </h2>
+            <p class="paragraph neutral-100">While offering amazing value</p>
+            <h2 class="h2 neutral-100 mb-4">The Simple, Premium & Cool ways to get married!</h2>
             <p class="body-3 neutral-100 mb-4">Your wedding ceremony should be a very special moment in your lives.
                So our focus is on giving you, the perfect simple ceremony - with the best celebrants & locations.
                We then streamline the rest - keeping it simple, fast and great value. Standard ceremonies from just $270.
