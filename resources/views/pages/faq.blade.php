82ff90d7a6a566f9cb6562f5a32ad394ca54b119
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-06-13 18:52:00 +0530

    why sc html

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -5,7 +5,7 @@
          <div class="col-md-6">
             <p class="paragraph netural-100 mt-4">FAQ</p>
             <h1 class="h1 netural-100 mb-4">Frequently Asked Questions (FAQ) about getting married</h1>
-            <p class="body-3">Below is a list of Frequently Asked Questions (FAQ) about getting married in Australia. If you have any questions please feel free to call or email us, so one of our experienced marriage celebrants can help you</p>
+            <p class="body-3-regular">Below is a list of Frequently Asked Questions (FAQ) about getting married in Australia. If you have any questions please feel free to call or email us, so one of our experienced marriage celebrants can help you</p>
          </div>
          <div class="col-md-5 offset-md-1">
             <img class="img-fluid" src="/images/faq-hero.png" alt="Faq Hero">
