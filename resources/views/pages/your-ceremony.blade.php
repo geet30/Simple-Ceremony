5ae6ea0a471f3675c4e631a981cc5ca214f45029
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-06-14 13:32:07 +0530

    why-sc-page-html

---
+++ resources/views/pages/your-ceremony.blade.php
@@ -0,0 +1,294 @@
+@extends('layouts.master') @section('content')
+<section class="hero pb-100">
+   <div class="container">
+      <div class="row">
+         <div class="col-12">
+            <p class="paragraph netural-100 text-center mt-4">Your special day</p>
+            <h1 class="h1 netural-100 text-center mb-4">Make your special wedding with us</h1>
+            <p class="col-10 mx-auto text-center mb-5 pt-2 body-3-regular">With many years experience, we pride ourselves on ensuring your wedding ceremony has the warmth and personal feel that you would want and expect, at this important moment in your lives.<br><br>
+               It is our role not only to ensure all runs smoothly but importantly that you are both relaxed and can really enjoy your moment.</p>
+            <img src="images/you-ceremonies-hero.png" class="img-fluid" alt="Your Ceremonies Hero Image"> 
+         </div>
+      </div>
+   </div>
+</section>
+<section class="marrige-steps pb-100">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="h2 mb-75 neutral-100 text-center">On the day of your Marriage Ceremony</h2>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 1</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/Wedding.svg" class="" alt="Wedding">
+                  </div>
+                  <h3 class="text-white h3 mt-4">On the day of your ceremony - the first thing you have to do is SMILE</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">You are going to have a fabulous day!<br><br>
+                     By now everything should be in place to ensure your marriage ceremony will run smoothly<br><br>
+                     But for your comfort, you may want to have a quick look at this <a href="#" class="text-white button-1">Checklist</a>
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 2</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/Document.svg" class="" alt="Document">
+                  </div>
+                  <h3 class="text-white h3 mt-4">What you will need to bring</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">In general, you will need to bring original copies of;<br>
+                     The Notice of Intended Marriage (NoIM) form you signed & had witnessed by an Authorised Person<br>
+                     ID's used at lodgement of the NoIM (Passport OR birth certificate and drivers licence.)<br>
+                     Certificates relating to previous marriage/s,  if applicable. eg. divorce/ death certificate. ALSO, if these documents are not in English you will need to also bring NAATI authorised translations.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 3</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/hands.svg" class="" alt="Hands">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Who you will need to bring</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">In general, you will need to bring;<br><br>
+                     Each other<br>
+                     2 witnesses - they can be anyone as long as they are over 18 years old (and speak English)<br>
+                     They do not require any ID<br>
+                     In addition, if either of you do not speak or understand English you will need to bring an interpreter - this can be anyone as long as they are over 18 and are fluent in your language and English (and they are not either of the couple getting married). They are required to complete a<br><br>
+                     <a href="#" class="text-white button-1">Certificate of Faithful Performance</a><br><br>
+                     this must be completed prior to the ceremony.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="marrige-steps bg-turquoise-20 pt-100 pb-80">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="h2 mb-75 neutral-100 text-center">On arrival at your wedding ceremony</h2>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 1</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/clock-icon.svg" class="" alt="Clock">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Plan to arrive 10 minutes prior to your booking time</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">This is a great way to avoid stress - not running late<br><br>
+                     Don't forget to take into account; traffic, parking and maybe even getting (a little) lost.<br><br>
+                     If you ceremony is in The Studio, as there is no waiting room, please wait outside until your scheduled time.<br><br>
+                     It is also wise to ensure your guests have directions (share the Google Pin with them) and suggest they arrive a little early too.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 2</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/file-icon.svg" class="" alt="Document">
+                  </div>
+                  <h3 class="text-white h3 mt-4">What you will need to bring</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">We will be ready for you at your scheduled time - with a big smile.<br><br>
+                     At this stage we will take your original NoIM and sight your other documents  (if we haven't already). <br><br>
+                     ID's used at lodgement of NoIM (Passport Or Birth Certificate and Drivers Licence.<br>
+                     Certificate relating to previous marriages (if applicable eg. Divorce/ Death Certificates<br>
+                     We'll have prepared all documents, you will need to sign for you.<br><br>
+                     Before the ceremony you will sign the  Declaration of No Legal Impediment to Marriage form and afterward your Certificate of Marriage.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 3</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/couple-icon.svg" class="" alt="Couple">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Who you will need to bring</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">We'll suggest how the ceremony will flow, including;<br><br>
+                     if outside, the specific location where you would like to stand<br>
+                     how you would like to start<br>
+                     if you have a customised ceremony*<br>
+                     who has the rings (if applicable) etc.<br>
+                     *Click below to see a Sample Ceremony. This will give you an idea of the sort of ceremony that will be delivered and the legal things that we and YOU must say.<br><br>
+                     You're able to personalise your ceremony, if you would like to. No need to email it to us - just give us a copy on the day of your ceremony.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-12 text-center mt-60">
+            <a href="#" class="theme-btn primary-btn d-inline-block">See sample ceremony</a>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="marrige-steps pt-100 pb-100">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="h2 mb-75 neutral-100 text-center">Your ceremony</h2>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 1</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/Wedding.svg" class="" alt="Wedding">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Starting your ceremony</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">Or you may want to walk down the aisle together or even arrive separately. We can arrange all of this for you on the day.<br><br>
+                     Arriving Separately. If this is the case - not a problem. If one of you is at the designated location at the scheduled time (with all the documents and ID) we can start the paperwork (with only one of you). Then we can go to where the other party is (such as waiting in the car or around the corner) and finalise the paperwork, before returning to the location ready to start the ceremony - ready for the "grand entrance". This then means  you won't spoil the surprise of seeing each other for the "first time".
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 2</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/Document.svg" class="" alt="Document">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Your ceremony</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">Once you and your guests are ready we will start your ceremony.<br><br>
+                     Our aim is to make your ceremony a very special moment in your lives and your guests too.<br><br>
+                     We will deliver a warm and relaxed ceremony (or deliver the ceremony you have prepared)
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 3</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/certificate-icon.svg" class="" alt="Certificate">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Signing your Certificate of Marriage</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">Following your ceremony, we will arrange for you and your two witnesses (they can be anyone as long as they are over 18 years old and speak English, they do not require any ID) to sign your certificates.<br><br>
+                     We'll provide a small table/ stand for you to sign on.<br><br>
+                     We will then present you with your Certificate of Marriage.<br><br>
+                     At this stage we are always happy to take a group photo for you.<br><br>
+                     And then it 's time to CELEBRATE.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="marrige-steps bg-turquoise-20 pt-100 pb-80">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="h2 mb-75 neutral-100 text-center">On arrival at your wedding ceremony</h2>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 1</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/register-icon.svg" class="" alt="Register">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Registering your Marriage</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">While you are celebrating, we will register your marriage - the same day - online with Births, Deaths and Marriages NSW - the official government record keeper of marriages.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 2</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/small-doc-icon.svg" class="" alt="Document">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Getting your registered Marriage Certificate</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">Your registered Marriage Certificate can be ordered on-line or picked up from BDM NSW - on the same day OR the next business day.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Step 3</span>
+               <div class="steps-name flex-column d-flex mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/dark-heart.svg" class="" alt="Heart">
+                  </div>
+                  <h3 class="text-white h3 mt-4">Live happily ever after</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <p class="text-white body-3-medium ">We will be in touch the next day to confirm that your marriage has been registered.<br><br>
+                     We also love any feedback you can provide, great or otherwise.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-12 text-center mt-60">
+            <a href="#" class="theme-btn primary-btn d-inline-block">Getting your registered Marriage Certificate</a>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="py-100">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-5 mb-4 mb-md-0">
+            <img src="images/permium.png" alt="permium" class="img-fluid">
+         </div>
+         <div class="col-md-7">
+            <p class="paragraph neutral-100">While offering amazing value</p>
+            <h2 class="h2 neutral-100 mb-4">The Simple, Premium & Cool ways to get married!</h2>
+            <p class="body-3 neutral-100 mb-4">Your wedding ceremony should be a very special moment in your lives.
+               So our focus is on giving you, the perfect simple ceremony - with the best celebrants & locations.
+               We then streamline the rest - keeping it simple, fast and great value. Standard ceremonies from just $270.
+            </p>
+            <p class="body-1 mb-4 turquoise-100">We also have add ons: chairs, decorations, photographer, flowers, wedding rings and a venue to celebrate!
+            </p>
+            <p class="body-1 text-black mb-5"> One Stop - for your perfect wedding!</p>
+            <div class="d-lg-flex"> 
+               <a href="#" class="theme-btn primary-btn me-lg-4 d-flex justify-content-center mb-3 mb-lg-0">Make a booking</a> 
+               <a href="#" class="theme-btn primary-btn-border d-flex justify-content-center">See checklist to get married</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+@endsection
