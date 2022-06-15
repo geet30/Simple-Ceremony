baf95a8dae708a4f8f57e920aab35dc02363d100
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-06-15 19:24:13 +0530

    complete-work-html

--- resources/views/pages/complete-paperwork.blade.php
+++ resources/views/pages/complete-paperwork.blade.php
@@ -11,7 +11,7 @@
             <p class="body-3-regular mb-4">Online on the Simple Ceremonies website.Signed & witnessed by an <span class="body-1">Authorised Person*</span>OR NOW via <span class="body-1">VIDEO witnessed</span> by us, and uploaded back to Simple Ceremonies at least <span class="body-1">ONE CALENDAR MONTH PRIOR</span> to your ceremony date</p>
             <div class="d-flex">
                <a href="#" class="theme-btn primary-btn me-4">Set-up NoIM here</a>
-               <a href="#details-information" class="theme-btn primary-btn-border ">Read detail information</a>
+               <a href="#details-information" class="theme-btn primary-btn-border">Read detail information</a>
             </div>
          </div>
          <div class="col-md-6">
@@ -20,7 +20,7 @@
       </div>
    </div>
 </section>
-<section class="bg-turquoise-20 py-100">
+<section id="details-information" class="bg-turquoise-20 py-100">
    <div class="container">
       <div class="row">
          <div class="col-md-5">
@@ -33,12 +33,194 @@
       </div>
    </div>
 </section>
-<section class="note">
+<section class="note mt-50">
    <div class="container">
       <div class="row">
          <div class="col-md-10 mx-auto">
-            <p class="body-1"><span>Note :</span><br>
+            <p class="speacial-note body-2 bg-turquoise-20 mb-0"><span class="body-1">Note :</span><br>
             The Attorney General of Australia now permits NoIMs to be witnessed by an Authorised Person via video, in Australia only.</p>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="marrige-steps pt-100 pb-80">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h2 class="h2 mb-5 neutral-100 text-center">An Authorised Person Includes the following:</h2>
+         </div>
+         <div class="col-lg-6 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Authorised persons 1</span>
+               <div class="steps-name d-flex align-items-center mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/australia-map.svg" class="" alt="Map">
+                  </div>
+                  <h3 class="text-white h3 mb-0 mt-1">In Australia</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <h4 class="h4 text-white mb-4">Simply select, online</h4>
+                  <ul class="list-unstyled mb-0">
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">An authorised celebrant</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">A justice of the peace</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">A barrister or solicitor</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">A legally qualified medical practitioner<br>
+                           <span class="body-3">(these include medical doctors & dentists only - not Registered Nurses)</span></p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">A member of the Australian Federal Police or the police force of a State or Territory</p>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-6 mb-4">
+            <div class="steps">
+               <span class="step-counter text-white h4 d-inline-block">Authorised persons 2</span>
+               <div class="steps-name d-flex align-items-center mt-4 pt-5 mb-3">
+                  <div class="step-icon me-4 d-flex align-items-center justify-content-center">
+                     <img src="/images/earth-icon.svg" class="" alt="Outside Australia">
+                  </div>
+                  <h3 class="text-white h3 mb-0 mt-1">Outside Australia</h3>
+               </div>
+               <div class="step-description mt-4">
+                  <h4 class="h4 text-white mb-4">if a party signs the Notice outside Australia</h4>
+                  <ul class="list-unstyled mb-0">
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">An Australian Consular Officer, an Australian Diplomatic Officer</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">a notary public, an employee of the Commonwealth authorised under paragraph of The Consular Fees Act 1955</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-1">An employee of the Australian Trade Commission authorised under paragraph of The Consular Fees Act 1955</p>
+                     </li>
+                     <li class="d-flex align-items-start mt-3">
+                        <img style="top:3px;" class="me-3 position-relative invisible" src="images/green-check.svg" alt="Check Icon">
+                        <p class="text-white mb-0 body-3">Note: For the definitions of Australian Consular Officer and Australian Diplomatic Officer, see section 2 of the Consular Fees Act 1955</p>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="pb-100">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-6">
+            <h2 class="h2 netural-100 mb-4">Detail instructions to complete your Notice of Intended Marriage online and upload it back to us.</h2>
+            <p class="body-3-regular">NB. Instructions are provided online when completing the NoIM so you need not read the details below - only if you are looking for clarification.</p>
+         </div>
+         <div class="col-md-5 offset-md-1">
+            <img src="images/woman-signing-marriage-contract-closeup.png" alt="Sign" class="img-fluid">
+         </div>
+      </div>
+   </div>
+</section>
+<section class="info-steps">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <ul class="events p-0">
+               <li>
+                  <span class="step-point"></span>
+                  <div class="steps-information">
+                     <div class="steps-information-details bg-turquoise-20">
+                        <span class="step-counter text-white h4 d-inline-block">Step 1</span>
+                        <p class="body-3 netural-100 mb-0 mt-2">Click on the button above "Start your Notice of Intended Marriage on-line" and enter your details. Ensure all your details are 100% correct before you "Submit" the form. If you need to pause the process, to check details, not a problem, you can "save' the details and return at a later date. If you do this you will need to save the URL provided to you to re-access your details. (Full instructions, including video, will guide you)</p>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <span class="step-point"></span>
+                  <div class="steps-information">
+                     <div class="steps-information-details bg-turquoise-20">
+                        <span class="step-counter text-white h4 d-inline-block">Step 2</span>
+                        <p class="body-3 netural-100 mb-0 mt-2">Check all details are correct, when they are hit the "Submit" button. You will then receive an email within seconds with an attached PDF of your personalised Notice of Intended Marriage.</p>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <span class="step-point"></span>
+                  <div class="steps-information">
+                     <div class="steps-information-details bg-turquoise-20">
+                        <span class="step-counter text-white h4 d-inline-block">Step 3</span>
+                        <p class="body-3 netural-100 mb-0 mt-2">
+                           <span class="body-1">Signing & witnessing you NoIM.</span> Either with a hard copy of the NoIM you have printed or opened on your smart phone - you will need to sign (by pen or electronically) the form in front of an <span class="body-1">Authorised Person*</span> (see below for a list of authorised witnesses*). Note. this can now be via video (see instructions once you have completed the filling out of the NoIM online)<br><br>
+                           If both parties cannot cannot conveniently have their signature witnessed by the Authorised Person - one party with suffice.<br><br>
+                           NB. However, both parties will need to sign "Permission to lodge the Notice of Intended Marriage form" attached to the NoIM (see the last page) and upload it back with the signed & witnessed NoIM and other required docs such as IDs. Then on the day of your ceremony, the party who was unable to sign the NoIM will sign in front of their Celebrant, who will then witness it.<br><br>
+                           Then have the Authorised Witness sign. *
+                        </p>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <span class="step-point"></span>
+                  <div class="steps-information">
+                     <div class="steps-information-details bg-turquoise-20">
+                        <span class="step-counter text-white h4 d-inline-block">Step 4</span>
+                        <p class="body-3 netural-100 mb-0 mt-2">Click on the button above "Start your Notice of Intended Marriage on-line" and enter your details. Ensure all your details are 100% correct before you "Submit" the form. If you need to pause the process, to check details, not a problem, you can "save' the details and return at a later date. If you do this you will need to save the URL provided to you to re-access your details. (Full instructions, including video, will guide you)</p>
+                        <ul class="list-unstyled mb-0">
+                           <li class="d-flex align-items-start mt-3">
+                              <img style="top:3px;" class="me-3 position-relative" src="/images/green-success.svg" alt="Check Icon">
+                              <p class="body-3 mb-0 netural-100">Signed & Witnessed NoIM.</p>
+                           </li>
+                           <li class="d-flex align-items-start mt-3">
+                              <img style="top:3px;" class="me-3 position-relative" src="/images/green-success.svg" alt="Check Icon">
+                              <p class="body-3 mb-0 netural-100">Each of your IDs (Passport OR original Birth Certificate AND government issued photo ID eg Drivers License).</p>
+                           </li>
+                           <li class="d-flex align-items-start mt-3 mb-3">
+                              <img style="top:3px;" class="me-3 position-relative" src="/images/green-success.svg" alt="Check Icon">
+                              <p class="body-3 mb-0 netural-100">Previous marriage documents (if applicable; Divorce or Death Certificate).</p>
+                           </li>
+                        </ul>
+                        <p class="body-3 netural-100 mb-0 mt-2"><span class="body-1">Note :</span><br>
+                           This needs to be done at least once calendar month prior to your ceremony date.</p>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <span class="step-point"></span>
+                  <div class="steps-information">
+                     <div class="steps-information-details bg-turquoise-20">
+                        <span class="step-counter text-white h4 d-inline-block">Step 5</span>
+                        <p class="body-3 netural-100 mb-0 mt-2">
+                           Within 24 hours you should receive confirmation of the Lodgement of your Notice of Intended Marriage.<br><br>
+                           Until this is received your Notice has not been lodged. <span class="body-1">If you have not heard back within 24 hours OR your it is within hours of it being "one calendar month prior to your ceremony date, please contact us to confirm the Notice has been received and can be lodged.</span>
+                        </p>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <span class="step-point"></span>
+                  <div class="steps-information">
+                     <div class="steps-information-details bg-turquoise-20">
+                        <p class="body-2 mb-0 mt-2">
+                           Note :<br><br>
+                           It is your responsibility to ensure that your NoIM is completed correctly and received one calendar month prior to your ceremony by a celebrant from Simple Ceremonies - this was agreed to when you made your booking and covered in our Terms.<br><br>
+                           Your Notice should not be considered as lodged until you have received an email from Simple Ceremonies stating this. If you do not receive this email, within 24 hours, please contact us on 0412 477 141 or michael@simpleceremonies.com.au to follow up at least once calendar month prior to your ceremony date - THIS IS CRITICAL.
+                        </p>
+                     </div>
+                  </div>
+               </li>
+             </ul>
          </div>
       </div>
    </div>
