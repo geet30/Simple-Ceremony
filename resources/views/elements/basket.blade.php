5699ceed48ae0712557e60dcf723afa19a4fa9d1
chetnamalhotra <csharma@crebos.online>
2022-06-17 19:01:36 +0530

    add one s

---
+++ resources/views/elements/basket.blade.php
@@ -0,0 +1,52 @@
+<div class="offcanvas offcanvas-end theme-offcanvas" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h2 id="offcanvasRightLabel" class="h3 neutral-100">Your basket</h2>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">Cancel</button>
+  </div>
+  <div class="offcanvas-body">
+ <form>
+ <table class="table">
+  <thead>
+    <tr>
+      <th class="col-5">Item</th>
+      <th class="col-2">Qty</th>
+      <th class="col-2">Price</th>
+      <th class="col-2">Total</th>
+      <th class="col-1"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><input type="text" value="Flower package 1" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="1" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="$ 120" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="$ 120" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><img src="images/item-delete.svg" class="img-fluid"></td>
+    </tr>
+    <tr>
+      <td><input type="text" value="Gazebo" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="1" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="$ 130" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="$ 130" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><img src="images/item-delete.svg" class="img-fluid"></td>
+    </tr>
+    <tr>
+      <td><input type="text" value="Zoom streaming" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="1" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="$ 150" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><input type="text" value="$ 150" class="form-control body-1 netural-100" name="name" id="InputName" readonly></td>
+      <td><img src="images/item-delete.svg" class="img-fluid"></td>
+    </tr>
+  </tbody>
+  <tfooter>
+  <tr>
+  <td><span>&nbsp;</span></td>
+      <td colspan="2" class="total text-end">Grand total</td>
+      <td class="h4 neutral-100">$ 400</td>
+      <td>&nbsp;</td>
+    </tr>
+  </tfooter>
+</table>
+ </form>
+  </div>
+</div>
