b6a1aff7e8e7edc0eebf44f598203bf058e6a0be
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-06-24 16:27:09 +0530

    user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,22 +3,28 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="apple-touch-icon" sizes="180x180" href="images/favicon//apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
+      <meta name="theme-color" content="#E5E5E5">
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon//apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon/favicon-96x96.png">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <link href="css/app.css" rel="stylesheet">
-      <link href="custom-css/select-2.css" rel="stylesheet">
-      <link href="fonts/stylesheet.css" rel="stylesheet">
+      <link href="/css/app.css" rel="stylesheet">
+      <link href="/custom-css/select-2.css" rel="stylesheet">
+      <link href="/custom-css/slick.css" rel="stylesheet">
+      <link href="https://intl-tel-input.com/node_modules/intl-tel-input/build/css/intlTelInput.css?1549804213570" rel="stylesheet">
+      <link href="/custom-css/calendar.css" rel="stylesheet">
+      <link href="/fonts/stylesheet.css" rel="stylesheet">
    </head>
    <body class="antialiased" data-barba="wrapper">
       <main>
-        @yield('content')
+         @yield('content')
       </main>
    </body>
    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
-   <script src="js/app.js" defer></script>
+   <script type="text/javascript" src="https://kenwheeler.github.io/slick/slick/slick.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+   <script src="https://intl-tel-input.com/node_modules/intl-tel-input/build/js/intlTelInput.js?1549804213570"></script>
+   <script src="https://cdn.jsdelivr.net/gh/wrick17/calendar-plugin@master/calendar.min.js"></script>
+   <script src="/js/app.js" defer></script>
   </html>
