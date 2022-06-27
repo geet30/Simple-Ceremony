057ab60b66755fca5d0de950636cb9333dd6bff1
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-06-27 17:56:59 +0530

    add-on-changes

--- routes/web.php
+++ routes/web.php
@@ -116,13 +116,13 @@ Route::group(['prefix' => 'admin'], function () {
     });
 });
 Route::group(['prefix' => 'admin'], function () {
-
     Route::get('add-ons', function () {
         return view('admin.add-ons');
-
+    });
+});
+Route::group(['prefix' => 'admin'], function () {
     Route::get('location-detail', function () {
         return view('admin.location-detail');
-
     });
 });
 Auth::routes();
