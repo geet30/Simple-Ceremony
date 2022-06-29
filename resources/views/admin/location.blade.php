2aa2023992d212c9eed7fe3c3c49807a7facb92b
chetnamalhotra <csharma@crebos.online>
2022-06-29 10:29:21 +0530

    design

--- resources/views/admin/location.blade.php
+++ resources/views/admin/location.blade.php
@@ -8,25 +8,30 @@
       <div class="col-10 col-md-9 col-lg-10">
       @include('elements.panel-header')
          <div class="row mb-4">
+       
             <div class="col-md-8 col-xl-9 d-flex align-self-center">
-               <div class="dropdown">
-                  <a class="btn filtertoggle-btn">
-                  <img src="/images/location-page/filter-icon.svg" class="fliter-icon" alt="Filter Icon" >
-                  </a>
-                  <div class="select-with-checkbox">
-                     <select name="filter_by_categories" id="selectinput" class="js-placeholder-single-input form-control" >
-                        <option value="O1" data-badge="">Location</option>
-                        <option value="O2" data-badge="">Park</option>
-                        <option value="O3" data-badge="">Beach</option>
-                        <option value="O4" data-badge="">Harbour</option>
-                     </select>
-                  </div>
-               </div>
-               <div class="form-group has-search w-100 ms-4 position-relative align-self-center">
-                  <span class="fa fa-search form-control-feedback"></span>
-                  <input type="text" class="form-control" placeholder="Search">
+            <div class="dropdown">
+               <a class="btn">
+                  <img src="/images/location-page/filter-icon.svg" class="fliter-icon" alt="Filter Icon">
+                </a>
+                <div class="select-with-checkbox">
+                  <select name="filter_by_categories" id="selectinput" class="js-placeholder-single-input js-select2 form-control" multiple="multiple">
+                     <option value="O1" data-badge="">All</option>
+                     <option value="O2" data-badge="">Harbour</option>
+                     <option value="O3" data-badge="">Beach</option>
+                     <option value="O4" data-badge="">Park</option>
+                     <option value="O5" data-badge="">River</option>
+                     <option value="O6" data-badge="">Outdoors</option>
+                     <option value="O7" data-badge="">Indoors</option>
+                     <option value="O7" data-badge="">Unique</option>
+                  </select>
                </div>
             </div>
+            <div class="form-group has-search w-100 ms-4 position-relative">
+               <span class="fa fa-search form-control-feedback"></span>
+               <input type="text" class="form-control" placeholder="Search">
+            </div>
+         </div>
             <div class="align-self-center col-md-4 col-xl-3 d-grid mt-3 mt-md-0">
                <a class="theme-btn primary-btn d-flex justify-content-center" href="/admin/add-location">
                <img class="me-2" src="/images/icons/add.svg" alt="shopping-icon">
