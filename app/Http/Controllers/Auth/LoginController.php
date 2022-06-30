929b7fa5c0df6d8363257733e8607840845f73d2
‘neharayat-debug’ <neha.crebos@gmail.com>
2022-06-30 17:35:01 +0530

    admin login functionality

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
+use Redirect;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,28 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        if ($request->route_name=='admin-login') {
+            $role = 'Admin';
+            $redirection = 'admin-locations';
+        }
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user=Auth::user();
+            if($role==$user->roles->first()->name){
+                return redirect('/admin/location');
+            }else{
+                return Redirect::to('admin/login')->withErrors(['email' => 'Provided credentials does not have access of this panel']);
+            }
+        }
+        return Redirect::to('admin/login')->withErrors([
+                'email' => 'Credentials do not match our database.'
+            ]);
     }
 }
