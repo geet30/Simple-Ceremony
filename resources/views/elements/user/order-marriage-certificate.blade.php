375ab432ff156ce2461063a688d4ef6a35fc339e
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-07-18 16:27:22 +0530

    marrying-couple

---
+++ resources/views/elements/user/order-marriage-certificate.blade.php
@@ -0,0 +1,22 @@
+<div class="offcanvas offcanvas-end theme-offcanvas order-marriage-certificate" tabindex="-1" id="order_marriage_certificate"
+   aria-labelledby="offcanvasRightLabel">
+   <div class="offcanvas-header">
+      <p id="offcanvasRightLabel" class="h3 neutral-100">Order marriage certificate</p>
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close">Cancel</button>
+   </div>
+   <div class="offcanvas-body">
+      <form>
+         <div class="col-md-12 mb-4">
+            <label class="form-label small-text2">Your address</label>
+            <input type="text" placeholder="Add your address here" class="form-control body-1 netural-100" name="name">
+         </div>
+         <div class="col-md-12 mb-4">
+            <label class="form-label small-text2">Total fee</label>
+            <input type="text" placeholder="$ 100" class="form-control body-1 netural-100" name="name">
+         </div>
+         <div class="d-lg-flex">
+            <a href="#" class="theme-btn primary-btn me-lg-3 d-flex justify-content-center mb-3 mb-lg-0">Pay</a>
+         </div>
+      </form>
+   </div>
+</div>
---
+++ resources/views/elements/user/reschedule-ceremony-popup.blade.php
@@ -0,0 +1,64 @@
+<div class="modal whitout-header-modal reschedule-ceremony-popup fade" id="reschedule_ceremony_popup" tabindex="-1" aria-labelledby="exampleModalLabel"
+aria-hidden="true">
+<div class="modal-dialog modal-lg">
+   <div class="modal-content">
+      <div class="modal-body ">
+         <div class="row mb-4">
+            <div class="col-12 mb-3">
+               <div class="d-flex add-ons-detail">
+                  <div class="numberCircle align-self-center me-2">1</div>
+                  <p class="h4 text-black align-self-center mb-0">Change the date of your ceremony</p>
+               </div>
+            </div>
+            <div class="col-12 ">
+               <p class="body-3-medium text-black mb-4">
+                  To change the date or location of your ceremony the fee is $85.<br>
+                  This fee is based on you providing at least one month and two days notice, before your original ceremony date.<br>
+                  If the ceremony date you are wishing to change is less than one month and two days away, the full fee will need to be paid again. (per our <a href="#" class="turquoise-100 text-decoration-none">Terms</a>)
+               </p>
+            </div>
+         </div>
+         <div class="row mb-4">
+            <div class="col-12 mb-3">
+               <div class="d-flex add-ons-detail">
+                  <div class="numberCircle align-self-center me-2">2</div>
+                  <p class="h4 text-black align-self-center mb-0">Additional Fees</p>
+               </div>
+            </div>
+            <div class="col-12 ">
+               <p class="body-3-medium text-black mb-4">If you are "Upgrading" your location you will need to pay the difference between your original location and your new location. NB your fee will not be reduced if you are "Downgrading".If you do not provide one-month and two-days notice you will be required to pay the full fee again.<br>Before you nominate a new date for you to reschedule to, please ensure </p>
+               <ul class="addonspopup-list">
+                  <li>you have lodged your Notice of Intended Marriage at least one calendar month prior to this date.</li>
+                  <li>Check the location, date and time are available. To do this email wedo@simpleceremonies.com.au</li>
+               </ul>
+            </div>
+         </div>
+         <div class="row mb-4">
+            <div class="col-12 mb-3">
+               <p class="button-1 text-black align-self-center mb-0 ">Agree to Terms</p>
+            </div>
+            <div class="col-12 mb-3">
+               <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                  <label class="form-check-label body-3-medium text-black" for="invalidCheck2">
+                     I have checked with Simple Ceremonies and confirmed the location, date and time I wish to get married on are available.
+                  </label>
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="d-md-flex">
+               <div class="d-flex">
+                  <a role="button" class="theme-btn primary-btn  me-md-4 mb-3 mb-lg-0 w-100-sm"
+                     data-bs-toggle="offcanvas" data-bs-target="#reschedule_ceremony_sidebar"
+                     aria-controls="offcanvasRight">Continue</a>
+               </div>
+               <div class="d-flex">
+                  <a role="button" class="theme-btn primary-btn-border w-100-sm" data-bs-dismiss="modal" aria-label="Close">Cancel</a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+</div>
