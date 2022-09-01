00bdb2a77354f85552a1a012e00985afe9b15e2a
Garry <gurpreetsingh.crebos@gmail.com>
2022-09-01 18:15:52 +0530

    resolve the conflicts

--- app/Traits/Marriages/Methods.php
+++ app/Traits/Marriages/Methods.php
@@ -2,7 +2,7 @@
 
 namespace App\Traits\Marriages;
 
-use App\Models\{User,Booking,Locations};
+use App\Models\{User, Booking};
 
 trait Methods
 {
@@ -18,20 +18,18 @@ trait Methods
                     $query->select('name', 'id', 'price');
                 },
                 'booking.celebrant' => function ($query) use ($search) {
-                    $query->select('first_name', 'id');
-                    // ->where(function ($query) use ($search) {
-                    //     $query->where('first_name', 'like', '%' . $search . '%')
-                    //         ->orWhere('surname', 'like', '%' . $search . '%')
-                    //         ->orWhere('email', 'like', '%' . $search . '%');
-                    // });
+                    $query->select('first_name', 'id')
+                        ->where(function ($query) use ($search) {
+                            $query->where('first_name', 'like', '%' . $search . '%')
+                                ->orWhere('surname', 'like', '%' . $search . '%')
+                                ->orWhere('email', 'like', '%' . $search . '%');
+                        });
                 },
-            ])
-<<<<<<< HEAD
-                ->where(function ($query) use ($search) {
-                    $query->where('first_name', 'like', '%' . $search . '%')
-                        ->orWhere('surname', 'like', '%' . $search . '%')
-                        ->orWhere('email', 'like', '%' . $search . '%');
-                })->orderBy('id', 'DESC');
+            ])->where(function ($query) use ($search) {
+                $query->where('first_name', 'like', '%' . $search . '%')
+                    ->orWhere('surname', 'like', '%' . $search . '%')
+                    ->orWhere('email', 'like', '%' . $search . '%');
+            })->orderBy('id', 'DESC');
             dd($data->get()->toArray());
         } else {
             $data = User::role('User')
@@ -45,25 +43,6 @@ trait Methods
                     'booking.celebrant' => function ($query) {
                         $query->select('first_name', 'id');
                     },
-=======
-            ->where(function ($query) use ($search) {
-                $query->where('first_name', 'like', '%' . $search . '%')
-                    ->orWhere('surname', 'like', '%' . $search . '%')
-                    ->orWhere('email', 'like', '%' . $search . '%');
-            })->orderBy('id', 'DESC');
-            // dd($data->get()->toArray());
-        }else{
-            $data = User::role('User')->with([
-                'booking' => function ($query) {
-                    $query->select('booking_date', 'id', 'user_id', 'locationId', 'celebrant_id', 'first_couple_name', 'status', 'created_at', 'second_couple_name');
-                },
-                'booking.location' => function ($query) {
-                    $query->select('name', 'id', 'price');
-                },
-                'booking.celebrant' => function ($query) {
-                    $query->select('first_name', 'id');
-                },
->>>>>>> 7f7b570ef6cec88b3696b6c2115be510a69e9295
 
                 ])->select('name', 'id', 'phone', 'country_code', 'email')
                 ->orderBy('id', 'DESC');
@@ -73,11 +52,8 @@ trait Methods
     public static function marriage_detail($id = null)
     {
         $data =   Booking::with([
-            'user' => function($query){
-                $query->select('email','phone','country_code','id');
-            },
-            'location' => function($query){
-                $query->select('name','id','price');
+            'location' => function ($query) {
+                $query->select('name', 'id', 'price');
             },
             'celebrant' => function ($query) {
                 $query->select('first_name', 'id');
@@ -87,52 +63,5 @@ trait Methods
             $data = $data->where('id', $id);
         }
         return   $data;
-    }
-    public static function searchMarriageLocation($request){
-        $req_page = 1;
-        $records = 10;
-        $locations = self::marriages();
-
-        if($request->has('filter') && !empty($request->filter)){
-            if(in_array('0',$request->filter)){
-                $data =$locations;
-            }else{
-                $id = $request->filter;
-                $data = $locations->whereHas('booking', (function ($q) use ($id)
-                {
-                    $q->whereIn('locationId', $id);
-
-                }));
-            }
-            return $data->paginate($records, ['*'], 'page', $req_page);
-
-        }else{
-            return $locations->paginate($records, ['*'], 'page', $req_page);
-        }
-    }
-    public static function searchMarriages($request){
-
-        $req_page = 1;
-        $records = 10;
-        $whereClause = [];
-        $data = self::marriages();
-        // if ($request->has('status') && $request->filled('status')) {
-        //     $whereClause = [
-        //         ['status', '=', $request->status]
-        //     ];
-        // }
-        if($request->has('type') && $request->filled('type') &&  $request->type=='date'){
-            $request->search = date('Y-m-d',strtotime($request->search));
-        }
-        if ($request->has('search') && $request->filled('search')) {
-            $data = $data->whereHas('booking', (function ($q) use ($request)
-            {
-                $q->Where('booking_date', 'like', '%' . $request->search . '%');
-
-            }))->where($whereClause)->orderBy('id', 'DESC');
-        } else {
-            $data = $data->orderBy('id', 'DESC');
-        }
-        return $data->paginate($records, ['*'], 'page', $req_page);
     }
 }
