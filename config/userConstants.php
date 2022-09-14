b531e501fd4651372451c98d669f76f4aa13c7fa
Garry <gurpreetsingh.crebos@gmail.com>
2022-09-14 12:24:07 +0530

    working on user document

---
+++ config/userConstants.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'conjugal_status' => [
+        1 => 'New validly married',
+        2 => 'Divorced',
+        3 => 'Divorce pending',
+        4 => 'Widowed'
+    ],
+    'countries' => [
+        'australia' => [
+            'name' => 'Australia',
+            'states' => [
+                'NWS' => 'NWS',
+                'VIC' => 'VIC',
+                'ACT' => 'ACT',
+                'NT' => 'NT',
+                'WA' => 'WA',
+                'SA' => 'SA',
+                'TAS' => 'TAS'
+            ]
+        ],
+    ],
+    'gender' => [
+        1 => 'Male',
+        2 => 'Female',
+        3 => 'Non-binary',
+        4 => 'X'
+    ]
+];
