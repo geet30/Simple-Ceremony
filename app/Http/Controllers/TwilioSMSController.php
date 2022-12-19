07b4c0ec5e3f167628304de24fdb89e2b68d82bd
geetanjali.crebos@gmail.com <geetanjali.crebos@gmail.com>
2022-12-19 13:43:19 +0530

    link update

--- app/Http/Controllers/TwilioSMSController.php
+++ app/Http/Controllers/TwilioSMSController.php
@@ -27,7 +27,7 @@ class TwilioSMSController extends Controller
             $account_sid = getenv("TWILIO_SID");  
             $auth_token = getenv("TWILIO_TOKEN");
             $twilio_number = getenv("TWILIO_FROM");
-            $message = "This is testing from ItSolutionStuff.com";
+            $message = " Hi {Person 1 Preferred Name} and {Person 2 Preferred Name}, All set for your ceremony at {Start time and Date} at {Location} - Google Pin {Locatin google pin}. Click here to log in to your portal to ensure all is set - [Link to sign in to Portal]. If you have any questions on the day please call me on {marriage celebrants phone number}. See you soon, {Marriage celebrants name}.";
 
             $booking = Booking::with('user')->where('booking_date', '=', $check_date)->get();
             $count = 0;
