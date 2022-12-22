d2d33c547d4bdb7d6fe95f6bb5943df1d6455066
‘surindercrebos’ <surinder.crebos@gmail.com>
2022-12-22 10:41:04 +0530

    calendar-fixes

--- public/fullcalendar/init.js
+++ public/fullcalendar/init.js
@@ -16,75 +16,54 @@ function initCalander(targetId,bookingData) {
     console.log('=============BOOKING DATA=======================');
     console.log(bookingData);
     console.log('====================================');
-    var availibitySlots = [];
-    var sortBookingData = [];
     
-    // bookingData.forEach(element => {
-    //     let availableObj = {
-    //         id: element.id,
-    //         // title: element.location.name + ' $ '+(parsePrice(element.admin_fee) + parsePrice(element.your_fee) + parsePrice(element.location_fee)),
-    //         title: element.location_name,
-            
-    //         // start: element.dates.start_date,
-    //         // end: element.dates.end_date,
-    //         start :element.booking_date,
-    //         end :element.booking_date,
-    //         daysOfWeek:[findWeekDay(element.dayText)],
-    //         classNames:['calendar-availability-ns'],
-    //         startTime:element.booking_start_time,
-    //         endTime: element.booking_end_time,
-    //     };
-    //     sortBookingData.push({
-    //         id: element.id,
-    //         // title: element.location.name + ' $ '+(parsePrice(element.admin_fee) + parsePrice(element.your_fee) + parsePrice(element.location_fee)),
-    //         title: element.location_name,
-    //         start :element.booking_date,
-    //         end :element.booking_date,
-    //         daysOfWeek:[findWeekDay(element.dayText)],
-    //         // allDay:true,
-    //         classNames:['calendar-slots-booked-ns'],
-    //         // displayEventTime:false
-    //         startTime:element.booking_start_time,
-    //         endTime: element.booking_end_time,
-    //     });
-    //     sortBookingData.push(availableObj);
-    // });
-    // sortBookingData.forEach(av => {
-
-    // })
-    setbookingData = [];
-   
-   
+    
+    var setbookingData = [];
+    
     $.each(bookingData,function(booking_date,response){
-        var price_info = availability =ceremonies_count=0;
-        // console.log(response.slotsInfo.availability_slots_count,'safdsf');
-        $.each(response.data,function(field_name,element){
-            if(element.price_info){
-                price_info = JSON.parse(element.price_info);
-            }        
+        ceremonies_booked_count = 0;
+        var price =price_info = availability =0;
+        availability = response.slotsInfo.availability_slots_count +'/' + response.slotsInfo.total_slots;
+        ceremonies_booked_count = response.slotsInfo.ceremonies_count;
+        console.log('eachtime',ceremonies_booked_count);
+        item = {}
+        item['htmlcustom'] = `<ul><li class="fc-event-availability">${availability} availability</li>`;
+        $.each(response.available_slots,function(field_name,element){
+            var price =  ' $ '+(parsePrice(element.admin_fee) + parsePrice(element.your_fee) + parsePrice(element.location_fee));
             
-            availability = response.slotsInfo.availability_slots_count +'/' + response.slotsInfo.total_slots;
-        
-            ceremonies_count = response.slotsInfo.ceremonies_count;
-           
-            console.log(element.ceremonies_count,'ceremonies_count');
-            var price =  ' $ '+(parsePrice(price_info.admin_fee) + parsePrice(price_info.your_fee) + parsePrice(price_info.location_fee));
-            
-            item = {}
-            item ["id"] = element.id;
-            item ["title"] = element.location_name;
-    
-            item ["start"] = booking_date+'T'+element.booking_start_time;
-            item ["end"] = booking_date+'T'+element.booking_end_time;
+            item ["start"] = booking_date+'T'+element.start_time;
+            item ["end"] = booking_date+'T'+element.end_time;
             item ["classNames"] =['calendar-availability-class'];
             item ["price"] =price;
             item ["availability"] =availability+' availability';
-            item ["ceremonies"] =ceremonies_count+' ceremonies';
-            item ["couple"] =element.first_couple_name+' & '+element.second_couple_name;
-            item ["start_time"] =element.booking_start_time;
-            item ["location"] =element.location_name; 
-            setbookingData.push(item);
+            item['htmlcustom'] += `<li class="fc-event-availability-slots">${element.start_time} - ${element.end_time}</li>
+            <li class="fc-event-availability_location">${element.location.name}</li>
+            <li class="fc-event-availability_slot_price">${price}</li>
+           `;
         });
+        item['htmlcustom'] += `<li class="fc-event-ceremonies_count mt-4 mb-2">${ceremonies_booked_count} ceremonies</li>`;
+        $.each(response.data,function(field_name,element2){
+            var time_format= moment(element2.booking_date+' '+element2.booking_start_time).format('HH:mm');
+            console.log('time_format',time_format);
+            item ["ceremonies"] =ceremonies_booked_count+ ' cermonies';
+            item ["couple"] =element2.first_couple_name+' & '+element2.second_couple_name;
+            item ["start_time"] =element2.booking_start_time;
+            item ["location"] =element2.location_name;
+           
+            if(ceremonies_booked_count == 0){
+                item['htmlcustom'] += `<li class="fc-event-no_cermeony">You don't have ceremony</li>`;
+            }else{
+                item['htmlcustom'] += `
+                <li class="fc-event-booking_couple">${element2.first_couple_name+' & '+element2.second_couple_name}</li>
+                <li class="fc-event-booking_start">${time_format}</li>
+                <li class="fc-event-booking_location mb-2">${element2.location_name}</li>
+                `;
+            }
+
+            
+        });
+        item['htmlcustom'] += '</ul>';
+        setbookingData.push(item);
         
            
     })
@@ -96,21 +75,16 @@ function initCalander(targetId,bookingData) {
         timeZone: 'UTC',
         events:setbookingData,
        
+       
         eventDidMount: function(info) {
-
             // console.log(info.view.type,'type');
             // if(info.view.type == 'dayGridMonth' || info.view.type == 'timeGridWeek' || info.view.type == 'timeGridDay' ){
-                var element = $(info.el);
-                console.log(info.event.extendedProps.price);
-                
-                element.find('.fc-daygrid-event-dot').before(`<div class="fc-event-availability mb-2">${info.event.extendedProps.availability}</div>`);
-                element.find('.fc-event-time').after(`<div class="fc-event-price mb-2">${info.event.extendedProps.price}</div>`);
-                // element.find('.fc-event-time').after(`<div class="fc-event-location mb-2">${info.event.extendedProps.location}</div>`);
-                element.find('.fc-event-price').after(`<div class="fc-event-ceremonies mb-2">${info.event.extendedProps.ceremonies}</div>`);
-                element.find('.fc-event-ceremonies').after(`<div class="fc-event-couple mb-2">${info.event.extendedProps.couple}</div>`);
-                element.find('.fc-event-couple').after(`<div class="fc-event-starttime mb-2">${info.event.extendedProps.start_time}</div>`);
-                
-            //}
+            var element = $(info.el);
+            element.html('');
+            // $('.calendar-availability-class').html('');
+
+            element.append(info.event.extendedProps.htmlcustom);
+            
             
         },
 
