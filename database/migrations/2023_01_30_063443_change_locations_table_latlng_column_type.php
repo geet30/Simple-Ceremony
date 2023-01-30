80397a0c7091612bd9811d5f2ec04830cdd69fc0
geetanjali.crebos@gmail.com <geetanjali.crebos@gmail.com>
2023-01-30 15:21:28 +0530

    migration changes

--- database/migrations/2023_01_30_063443_change_locations_table_latlng_column_type.php
+++ database/migrations/2023_01_30_063443_change_locations_table_latlng_column_type.php
@@ -14,11 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::table('locations', function (Blueprint $table) {
-            $table->decimal('latitude', 10, 8)->nullable()->change();
+            $table->decimal('latitude', 10, 8)->nullable()->default('0.00')->change();
             $table->decimal('longitude', 11, 8)->nullable()->change();
         });
         Schema::table('request_locations', function (Blueprint $table) {
-            $table->decimal('latitude', 10, 8)->nullable()->change();
+            $table->decimal('latitude', 10, 8)->nullable()->default('0.00')->change();
             $table->decimal('longitude', 11, 8)->nullable()->change();
         });
     }
